fd2cb0e141567fc0c511ae957cd0ef63ef5bddfa
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-25 22:26:59 +0330

    Add project fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,30 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono as GeistMono } from "next/font/google";
+import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
+  weight: "variable",
 });
 
-const geistMono = GeistMono({
-  variable: "--font-geist-mono",
+const spaceGrotesk = SpaceGrotesk({
+  variable: "--font-space-grotesk",
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "Stack Overflow for but better!",
+  description: "A better version of Stack Overflow",
 };
 
 const RootLayout = ({ children }: LayoutProps<"/">) => {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.className} ${spaceGrotesk.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
